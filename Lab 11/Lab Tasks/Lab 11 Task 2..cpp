dcb5214662719241033ee91ff12c956bd30fb886
awaisbacha0 <awaisbacha327@gmail.com>
2026-05-19 01:54:56 +0500

    Add files via upload

---
+++ Lab 11/Lab Tasks/Lab 11 Task 2..cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Declare and initialize string variables
+    string firstName = "Abbas";
+    string lastName = "Fazal";
+
+    // Concatenate the strings
+    string fullName = firstName + " " + lastName;
+
+    // Display the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
